de36dcbab3238dbdd720d35aec3f7e5a737fcdd8
Marta Rg <martaroguerra@gmail.com>
2023-09-06 21:19:38 +0200

    * Better front

--- main.py
+++ main.py
@@ -33,19 +33,21 @@ def activate_button(b):
             st.session_state[i] = False # We save False for the rest
 
 
-# st.set_page_config(layout="wide")
+st.set_page_config(layout="centered")
 st.title('Countries view')
 
-region_options = [i['region_name'] for i in requests.get(f'{api_url}/regions').json()]
-region_selected_name = st.selectbox('Region', region_options)
-region_selected_id = pd.DataFrame(
-    requests.get(
-        f'{api_url}/regions'
-    ).json()).set_index('region_name').to_dict()['region_id'][region_selected_name]
-country_options = [
-    i['country_name'] for i in requests.get(f'{api_url}/countries').json()
-    if i['region_id'] == region_selected_id
-]
+# region_options = [i['region_name'] for i in requests.get(f'{api_url}/regions').json()]
+# region_selected_name = st.selectbox('Region', region_options)
+# region_selected_id = pd.DataFrame(
+#     requests.get(
+#         f'{api_url}/regions'
+#     ).json()).set_index('region_name').to_dict()['region_id'][region_selected_name]
+country_options = [i['country_name'] for i in requests.get(f'{api_url}/countries').json()]
+
+# country_options = [
+#     i['country_name'] for i in requests.get(f'{api_url}/countries').json()
+#     if i['region_id'] == region_selected_id
+# ]
 country_selected_name = st.selectbox('Country', country_options)
 country_selected_id = pd.DataFrame(
     requests.get(f'{api_url}/countries').json()
@@ -96,7 +98,7 @@ if st.session_state.politics:
             if j is not None:
                 if i == 'government_type':
                     st.header('Government structure', divider='blue')
-                    st.write('##')
+
                     st.subheader(j)
                 elif i == 'conflict_type':
                     st.write("#")
@@ -122,7 +124,8 @@ if st.session_state.politics:
 
 elif st.session_state.footprint:
 
-    st.subheader('General')
+    st.header('General footprint indicators', divider='green')
+    st.write('##')
     footprint = requests.get(f'{api_url}/countries_footprint').json()
     footprint_countries = footprint['countries']
     w = 0
@@ -213,7 +216,7 @@ elif st.session_state.gender:
             c1, c2 = st.columns([1, 1])
 
             with c1:
-                st.subheader('Global Gender Gap Index:', divider='rainbow')
+                st.subheader('Global Gender Gap Index:', divider='violet')
                 st.header(i['global_ggi'])
 
             with c2:
@@ -254,30 +257,45 @@ elif st.session_state.gender:
 
 elif st.session_state.general:
 
+    general_info = requests.get(f'{api_url}/country/{country_selected_id}/general').json()
+    st.header(country_selected_name, divider='rainbow')
     try:
-        general_info = requests.get(f'{api_url}/country/{country_selected_id}/general').json()
-        st.header(country_selected_name)
+        st.write('.'.join(general_info['summary'].split('.')[:7]) + '...')
+        st.write('#')
+    except:
+        pass
+
+    if general_info ['summary'] is None:
+        st.info(f"Hi! I'm Wikipedia-API python library. I do make a lot of mistakes.. sorry. I cannot provide you"
+                 f" with information about {country_selected_name}. "
+                 f"However, check out this other data from restcountries api! :")
+        st.write('##')
+
+    c3, c4 = st.columns([1, 1])
+    with c3:
+        st.subheader('Capital:')
+        st.write(general_info['capital'])
+        st.subheader('Population:')
+        st.write(f"{round(general_info['population']/(1e6),2)} M")
         try:
-            st.write('.'.join(general_info['summary'].split('.')[:5]) + '.')
-            st.write('#')
+            st.markdown(f"**Languages:** {', '.join([i['name'] for i in general_info['languages']])}")
         except:
             pass
+        st.markdown(f"**Area:** {int(general_info['area'])} km²")
 
-        c3, c4 = st.columns([1, 1])
-        with c3:
-            st.markdown(f"**Capital:** {general_info['capital']}")
-            st.markdown(f"**Population:** {int(general_info['population']/(1e6))} millions")
-            try:
-                st.markdown(f"**Languages:** {', '.join([i['name'] for i in general_info['languages']])}")
-            except:
-                pass
-            st.markdown(f"**Area:** {general_info['area']}")
+    with c4:
+        rel_country = st.selectbox('Population and size in terms of..', options=country_options)
+        rel_country_id = pd.DataFrame(
+            requests.get(f'{api_url}/countries').json()
+        ).set_index('country_name').to_dict()['country_id'][rel_country]
+        general_rel_info = requests.get(f'{api_url}/country/{rel_country_id}/general').json()
+        rel_pop = round(general_info['population']/general_rel_info['population'],2)
+        rel_size = round(general_info['area']/general_rel_info['area'],2)
+        st.subheader(f"{rel_pop} times")
+        st.write(f"the population of {rel_country}.")
+        st.subheader(f"{rel_size} times")
+        st.write(f"the size of {rel_country}.")
 
-        with c4:
-            st.write('more')
-
-    except:
-        st.write(default_country)
 
 
 elif st.session_state.economy:
@@ -302,7 +320,7 @@ elif st.session_state.economy:
                     st.header(f"{tgdp /(1e9)} B")
                 st.subheader(f"World share:  {round(100 * i['world_share'], 2)} %")
                 st.write("#")
-                st.subheader("GDP per capita", divider='violet')
+                st.subheader("GDP per capita", divider='blue')
                 source3 = pd.DataFrame([i['gdp_capita'], i['ppp_gdp_capita'], gdp['median_ppp_gdp_capita']],
                                        index=['Country GDP per capita', 'Per capita (PPP)',
                                               'Worldwide median (PPP)']).T.reset_index()
@@ -328,12 +346,33 @@ elif st.session_state.inequality:
     for i in countries_ineq:
         if i['country_id'] == country_selected_id:
             w = 1
+            st.header('General indicators', divider='grey')
+            if i['gini_coefficient'] is not None:
+                st.info('The Gini index is a measure of statistical dispersion intended to '
+                        'represent the income inequality, the wealth inequality, or the consumption inequality '
+                        'within a nation or a social group. A gini value of 0% reflects maximum inequallity and'
+                        'one of 100% represents maximum equality '
+                        '(see its [definition](https://en.wikipedia.org/wiki/Gini_coefficient)).')
+            colss = st.columns([1,1,1])
+            toplot = [i['gini_coefficient'], i['richest_10_percent_share'], i['richest_1_percent_share']]
+            names = ['Gini index (%):', 'Richest 10% share', 'Richest 1% share']
+
+            a = 0
+            for l in range(3):
+                if toplot[l] is not None:
+                    with colss[a]:
+                        st.subheader(names[l])
+                        st.header(toplot[l])
+                        a+=1
+
+
             if isinstance(i['vulnerable_MP_pop'], float):
+                st.write('#')
                 st.subheader('Multidimensional poverty (MP)', divider='red')
                 st.warning(f"{country_selected_name} is a country with a multidimensional poverty problem. "
                            f"\nA person is considered to be MP poor when is deprived in a third or more of several indicators, "
                            f"concerning health, education and living standard (see [ref](https://ophi.org.uk/multidimensional-poverty-index/)).")
-                col7, col8 = st.columns([1,1])
+                col7, col8 = st.columns([1, 1])
 
                 with col7:
                     st.subheader('Population at MP risk:')
@@ -353,7 +392,7 @@ elif st.session_state.inequality:
                     ]
                 }
 
-                st.bar_chart(pd.DataFrame(pd.DataFrame(data).set_index('Names')), color=(0, 150, 0))
+                st.bar_chart(pd.DataFrame(pd.DataFrame(data).set_index('Names')), color=(0, 50, 100))
 
     if w == 0:
         st.write(default_country)
